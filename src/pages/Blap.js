d00c7579e05f3f2019d61b2b0d1407f770aae485
yu-jeffy <105130385+yu-jeffy@users.noreply.github.com>
2024-07-06 09:13:56 -0700

    blint mobile sizing

--- src/pages/Blap.js
+++ src/pages/Blap.js
@@ -1,72 +1,403 @@
 import React, { useState, useEffect, useRef } from 'react';
-import frames from '../utils/ascii_frames_with_blorm.json'; // Adjust the path as necessary
 import styles from '../styles/Blap.module.css';
-import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
+import LoadingBlap from '../components/LoadingBlap';
+import * as chainRegistry from 'chain-registry';
+import { SigningStargateClient } from '@cosmjs/stargate';
+// import { SkipRouter, SKIP_API_URL } from '@skip-router/core'
 
 const Blap = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [asciiFrames, setAsciiFrames] = useState([]);
-  const animationRef = useRef(null);
-  const containerRef = useRef(null);
-  const [fontSize, setFontSize] = useState(10);
-  const speed = 100; // Adjust this value for animation speed (in milliseconds)
+  /* const SkipClient = new SkipRouter({
+    apiURL: SKIP_API_URL,
+    getCosmosSigner: window.keplr.getOfflineSigner
+  });
+  */
 
-  // Function to calculate and resize ASCII art based on viewport dimensions
-  const resizeAsciiArt = (frame, width) => {
-    const lines = frame.split('\n');
-    const newLines = lines.map(line => line.padEnd(width, ' '));
-    return newLines.join('\n');
+  const [showLoading, setShowLoading] = useState(false);
+  const [assets, setAssets] = useState([]);
+  const [chains, setChains] = useState([]);
+  const [selectedSourceChain, setSelectedSourceChain] = useState(null);
+  const [selectedSourceAsset, setSelectedSourceAsset] = useState(null);
+  const [selectedDestinationChain, setSelectedDestinationChain] = useState(null);
+  const [selectedDestinationAsset, setSelectedDestinationAsset] = useState(null);
+
+  const [sourceAmount, setSourceAmount] = useState('');
+  const [destinationAmount, setDestinationAmount] = useState('');
+  const [sourceError, setSourceError] = useState('');
+  const [destinationError, setDestinationError] = useState('');
+
+  const [sourceChainSearch, setSourceChainSearch] = useState('');
+  const [sourceAssetSearch, setSourceAssetSearch] = useState('');
+  const [destinationChainSearch, setDestinationChainSearch] = useState('');
+  const [destinationAssetSearch, setDestinationAssetSearch] = useState('');
+
+  const [isSourceChainDropdownOpen, setIsSourceChainDropdownOpen] = useState(false);
+  const [isSourceAssetDropdownOpen, setIsSourceAssetDropdownOpen] = useState(false);
+  const [isDestinationChainDropdownOpen, setIsDestinationChainDropdownOpen] = useState(false);
+  const [isDestinationAssetDropdownOpen, setIsDestinationAssetDropdownOpen] = useState(false);
+
+  const [routeResponse, setRouteResponse] = useState(null); // State variable for storing route response
+  const [msgResponse, setMsgResponse] = useState(null); // State variable for storing msg response
+  const [addresses, setAddresses] = useState({}); // State variable for storing addresses
+
+  const sourceChainRef = useRef(null);
+  const sourceAssetRef = useRef(null);
+  const destinationChainRef = useRef(null);
+  const destinationAssetRef = useRef(null);
+
+  useEffect(() => {
+    const fetchChains = async () => {
+      try {
+        const response = await fetch('https://api.skip.money/v2/info/chains');
+        const data = await response.json();
+        const allChains = data.chains.sort((a, b) => a.chain_name.localeCompare(b.chain_name));
+        setChains(allChains);
+        const allAssets = chainRegistry.assets;
+        setAssets(allAssets);
+      } catch (error) {
+        console.error('Error fetching chains:', error);
+      }
+    };
+    fetchChains();
+  }, []);
+
+  const handleSourceChainChange = (chainName) => {
+    const chain = chains.find(c => c.chain_name === chainName);
+    setSelectedSourceChain(chain);
+    setSourceChainSearch(chain.chain_name);
+
+    const matchingAssets = chain.fee_assets.map(asset => {
+      const registryAsset = chainRegistry.assets.find(a => a.base === asset.denom);
+      return registryAsset ? { ...asset, ...registryAsset } : asset;
+    });
+
+    setAssets(matchingAssets);
+    setIsSourceChainDropdownOpen(false);
+  };
+
+  const handleSourceAssetChange = (assetBase) => {
+    const asset = assets.find(a => a.denom === assetBase);
+    setSelectedSourceAsset(asset);
+    setSourceAssetSearch(asset.denom);
+    setIsSourceAssetDropdownOpen(false);
+  };
+
+  const handleDestinationChainChange = (chainName) => {
+    const chain = chains.find(c => c.chain_name === chainName);
+    setSelectedDestinationChain(chain);
+    setDestinationChainSearch(chain.chain_name);
+
+    const matchingAssets = chain.fee_assets.map(asset => {
+      const registryAsset = chainRegistry.assets.find(a => a.base === asset.denom);
+      return registryAsset ? { ...asset, ...registryAsset } : asset;
+    });
+
+    setAssets(matchingAssets);
+    setIsDestinationChainDropdownOpen(false);
+  };
+
+  const handleDestinationAssetChange = (assetBase) => {
+    const asset = assets.find(a => a.denom === assetBase);
+    setSelectedDestinationAsset(asset);
+    setDestinationAssetSearch(asset.denom);
+    setIsDestinationAssetDropdownOpen(false);
+  };
+
+  const handleSourceAmountChange = (event) => {
+    setSourceAmount(event.target.value);
+  };
+
+  const handleDestinationAmountChange = (event) => {
+    setDestinationAmount(event.target.value);
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      sourceChainRef.current && !sourceChainRef.current.contains(event.target)
+      && sourceAssetRef.current && !sourceAssetRef.current.contains(event.target)
+      && destinationChainRef.current && !destinationChainRef.current.contains(event.target)
+      && destinationAssetRef.current && !destinationAssetRef.current.contains(event.target)
+    ) {
+      setIsSourceChainDropdownOpen(false);
+      setIsSourceAssetDropdownOpen(false);
+      setIsDestinationChainDropdownOpen(false);
+      setIsDestinationAssetDropdownOpen(false);
+    }
   };
 
   useEffect(() => {
-    // Function to handle resizing ASCII art frames
-    const handleResize = () => {
-      if (containerRef.current) {
-        const containerWidth = containerRef.current.clientWidth;
-        // const containerHeight = containerRef.current.clientHeight;
-        // const aspectRatio = containerHeight / containerWidth;
-        const fontSize = containerWidth / 375; // Adjust this value for font size
-        setFontSize(fontSize);
-        const width = Math.floor(containerWidth / fontSize);
-        // const height = Math.floor(containerHeight / fontSize / aspectRatio);
-        const resizedFrames = frames.map(frame => ({
-          ...frame,
-          background: resizeAsciiArt(frame.background, width),
-        }));
-        setAsciiFrames(resizedFrames);
-      }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-
-    handleResize();
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  useEffect(() => {
-    const animate = () => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % asciiFrames.length);
-      animationRef.current = setTimeout(animate, speed);
-    };
+  const filteredSourceChains = chains.filter(chain =>
+    chain.chain_name?.toLowerCase().includes(sourceChainSearch.toLowerCase())
+  );
 
-    if (asciiFrames.length > 0) {
-      animationRef.current = setTimeout(animate, speed);
+  const filteredSourceAssets = assets.filter(asset =>
+    asset.denom?.toLowerCase().includes(sourceAssetSearch.toLowerCase())
+  );
+
+  const filteredDestinationChains = chains.filter(chain =>
+    chain.chain_name?.toLowerCase().includes(destinationChainSearch.toLowerCase())
+  );
+
+  const filteredDestinationAssets = assets.filter(asset =>
+    asset.denom?.toLowerCase().includes(destinationAssetSearch.toLowerCase())
+  );
+
+  const handleRoute = async () => {
+    if (!selectedSourceAsset || !selectedSourceChain || !selectedDestinationAsset || !selectedDestinationChain) {
+      console.error("Please ensure all selections are made before routing.");
+      return;
     }
 
-    return () => clearTimeout(animationRef.current);
-  }, [speed, asciiFrames.length]);
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        amount_in: sourceAmount,
+        source_asset_denom: selectedSourceAsset.denom,
+        source_asset_chain_id: selectedSourceChain.chain_id,
+        dest_asset_denom: selectedDestinationAsset.denom,
+        dest_asset_chain_id: selectedDestinationChain.chain_id,
+        allow_unsafe: true,
+        allow_multi_tx: true,
+        smart_relay: true,
+        smart_swap_options: {
+          split_routes: true,
+          evm_swaps: true,
+        },
+        allow_swaps: true
+      })
+    };
+
+    try {
+      const response = await fetch('https://api.skip.money/v2/fungible/route', options);
+      const data = await response.json();
+      setRouteResponse(data); // Save response data in state
+
+      if (data.required_chain_addresses) {
+        const fetchedAddresses = await getKeplrAddresses(data.required_chain_addresses);
+        console.log('Addresses from route:', fetchedAddresses);
+        setAddresses(fetchedAddresses); // Save addresses in state
+        console.log('Addresses:', fetchedAddresses);
+      }
+    } catch (error) {
+      console.error('Error fetching route:', error);
+    }
+  };
+
+  const handleMsgSend = async () => {
+    if (!routeResponse) {
+      console.error("No route response to use for the message send.");
+      return;
+    }
+
+    const { source_asset_denom, source_asset_chain_id, dest_asset_denom, dest_asset_chain_id, amount_in, amount_out, operations } = routeResponse;
+    console.log('sending msg with:', source_asset_denom, source_asset_chain_id, dest_asset_denom, dest_asset_chain_id, amount_in, amount_out, addresses, operations);
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        source_asset_denom,
+        source_asset_chain_id,
+        dest_asset_denom,
+        dest_asset_chain_id,
+        amount_in,
+        amount_out,
+        address_list: Object.values(addresses),
+        operations,
+        slippage_tolerance_percent: "1"
+      })
+    };
+
+    try {
+      const response = await fetch('https://api.skip.money/v2/fungible/msgs', options);
+      const data = await response.json();
+      setMsgResponse(data); // Save msg response in state
+      console.log('Msg Response:', data);
+    } catch (error) {
+      console.error('Error sending msg:', error);
+    }
+  };
+
+  const getKeplrAddresses = async (chainIds) => {
+    if (!window.keplr) {
+      alert("Please install Keplr extension");
+      return;
+    }
+
+    const addresses = {};
+    for (const chainId of chainIds) {
+      await window.keplr.enable(chainId);
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const accounts = await offlineSigner.getAccounts();
+      addresses[chainId] = accounts[0].address;
+    }
+    console.log('Addresses from getKeplrAddresses:', addresses)
+    return addresses;
+  };
+
+  const handleExecuteRoute = async () => {
+    if (!routeResponse) {
+      console.error("No route response to use for the execute route.");
+      return;
+    }
+
+    /* const response = await SkipClient.executeRoute({
+      routeResponse,
+      addresses,
+      onTransactionCompleted: (chainID, txHash, status) => {
+        console.log(`Route completed on ${chainID} with tx hash: ${txHash} & status: ${status.state}`)
+      }
+    }) */
+  }; 
 
   return (
-    <div ref={containerRef} className={styles.container}>
-      <Navbar />
-      <div className={styles.background}>
-        <pre className={styles.pre} style={{ fontSize: `${fontSize}px`, lineHeight: `${fontSize}px` }}>
-          {asciiFrames[currentIndex]?.background}
-        </pre>
-      </div>
-      <div className={styles.titleContainer}>
-        {/*<h1 className={styles.title}>B L O R M</h1>*/}
+    <div className={styles.container}>
+      {showLoading && <LoadingBlap />}
+      <div className={styles.swapContainer}>
+        <div className={styles.swap}>
+          <div className={styles.swapInputSourceContainer}>
+            <div className={styles.swapInputDropdownContainer}>
+              <div className={styles.swapSectionInputSourceChain} ref={sourceChainRef}>
+                <input
+                  type="text"
+                  placeholder="Search Source Chain"
+                  value={sourceChainSearch}
+                  onChange={e => setSourceChainSearch(e.target.value)}
+                  className={styles.searchInput}
+                  onFocus={() => setIsSourceChainDropdownOpen(true)}
+                />
+                {isSourceChainDropdownOpen && (
+                  <div className={styles.dropdown}>
+                    {filteredSourceChains.map((chain) => (
+                      <div
+                        key={chain.chain_name}
+                        className={styles.dropdownItem}
+                        onClick={() => handleSourceChainChange(chain.chain_name)}
+                      >
+                        {chain.chain_name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <div className={styles.swapSectionInputSourceToken} ref={sourceAssetRef}>
+                <input
+                  type="text"
+                  placeholder="Search Source Asset"
+                  value={sourceAssetSearch}
+                  onChange={e => setSourceAssetSearch(e.target.value)}
+                  className={styles.searchInput}
+                  onFocus={() => setIsSourceAssetDropdownOpen(true)}
+                />
+                {isSourceAssetDropdownOpen && (
+                  <div className={styles.dropdown}>
+                    {filteredSourceAssets.map((asset) => (
+                      <div
+                        key={`${asset.chain_name}-${asset.denom}`}
+                        className={styles.dropdownItem}
+                        onClick={() => handleSourceAssetChange(asset.denom)}
+                      >
+                        {asset.denom}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className={styles.swapInputAmountContainer}>
+              <input
+                className={styles.swapInputAmount}
+                type="text"
+                placeholder="0.0"
+                value={sourceAmount}
+                onChange={handleSourceAmountChange}
+              />
+              {sourceError && <span className={styles.errorText}>{sourceError}</span>}
+            </div>
+          </div>
+          <div className={styles.swapInputDestinationContainer}>
+            <div className={styles.swapInputDropdownContainer}>
+              <div className={styles.swapInputDestinationChain} ref={destinationChainRef}>
+                <input
+                  type="text"
+                  placeholder="Search Destination Chain"
+                  value={destinationChainSearch}
+                  onChange={e => setDestinationChainSearch(e.target.value)}
+                  className={styles.searchInput}
+                  onFocus={() => setIsDestinationChainDropdownOpen(true)}
+                />
+                {isDestinationChainDropdownOpen && (
+                  <div className={styles.dropdown}>
+                    {filteredDestinationChains.map((chain) => (
+                      <div
+                        key={chain.chain_name}
+                        className={styles.dropdownItem}
+                        onClick={() => handleDestinationChainChange(chain.chain_name)}
+                      >
+                        {chain.chain_name}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+              <div className={styles.swapInputDestinationToken} ref={destinationAssetRef}>
+                <input
+                  type="text"
+                  placeholder="Search Destination Asset"
+                  value={destinationAssetSearch}
+                  onChange={e => setDestinationAssetSearch(e.target.value)}
+                  className={styles.searchInput}
+                  onFocus={() => setIsDestinationAssetDropdownOpen(true)}
+                />
+                {isDestinationAssetDropdownOpen && (
+                  <div className={styles.dropdown}>
+                    {filteredDestinationAssets.map((asset) => (
+                      <div
+                        key={`${asset.chain_name}-${asset.denom}`}
+                        className={styles.dropdownItem}
+                        onClick={() => handleDestinationAssetChange(asset.denom)}
+                      >
+                        {asset.denom}
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className={styles.swapInputAmountContainer}>
+              <input
+                className={styles.swapInputAmount}
+                type="text"
+                placeholder="0.0"
+                value={destinationAmount}
+                onChange={handleDestinationAmountChange}
+              />
+              {destinationError && <span className={styles.errorText}>{destinationError}</span>}
+            </div>
+          </div>
+        </div>
+        <div className={styles.swapAction}>
+          <button onClick={handleRoute} className={styles.routeButton}>Route</button>
+        </div>
+        {routeResponse && (
+          <div className={styles.responseContainer}>
+            <h3>Route Response</h3>
+            <pre>{JSON.stringify(routeResponse, null, 2)}</pre>
+            <button onClick={handleMsgSend} className={styles.msgButton}>Send Msg</button>
+            <button onClick={handleExecuteRoute} className={styles.executeRouteButton}>Execute Route</button>
+          </div>
+        )}
+        {msgResponse && (
+          <div className={styles.responseContainer}>
+            <h3>Msg Response</h3>
+            <pre>{JSON.stringify(msgResponse, null, 2)}</pre>
+          </div>
+        )}
       </div>
     </div>
   );
